5661332390d7dd64d061b5d814dc0ef6456c48d9
OlegPiskun <112425702+OlegPiskun@users.noreply.github.com>
2023-02-28 18:07:43 +0000

    Deploying to gh-pages from @ OlegPiskun/goit-js-hw-09@71f097f2148e34236288277f40ff3cc65e29d241 🚀

--- 01-color-switcher.1b36934c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.1b36934c.js.map
--- 01-color-switcher.975aab7e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;let d=null;t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));
-//# sourceMappingURL=01-color-switcher.975aab7e.js.map
